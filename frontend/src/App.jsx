6d7704f8132c9d4afa061fa16c037774a94476ac
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-03-01 07:28:26 +0100

    Add The Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
-// Import your AdminLayout if needed
-// import AdminLayout from "./components/Layout/AdminLayout";
+import Home from "./pages/Home";
+import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
+import ProductDetails from "./components/Products/ProductDetails";
+import Checkout from "./components/Cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
+import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
+import EditProductPage from "./components/Admin/EditProductPage";
+import OrderManagement from "./components/Admin/OrderManagement";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatpath: true }}>
+      <Toaster position="top-right" />
       <Routes>
         {/* Route for User Layout */}
-        <Route path="/" element={<UserLayout />} />
-
-        {/* Route for Admin Layout (Uncomment if needed) */}
-        {/* <Route path="/admin" element={<AdminLayout />} /> */}
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collections/:collection" element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} /> {/* Fixed: Removed backslash */}
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="order-confirmation" element={<OrderConfirmationPage />} />
+          <Route path="my-orders" element={<MyOrdersPage />} />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+        </Route>
+=        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement/>} />
+          <Route path="products" element={<ProductManagement/>} />
+          <Route path="products/:id/edit" element={<EditProductPage />} />
+          <Route path="orders" element={<OrderManagement/>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
