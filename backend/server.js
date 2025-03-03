9ca7c7f87affeb8d93b75d3b44813afcae5b6de3
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-03-03 11:31:26 +0100

    Entry Point

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv")
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
+const productRoutes = require("./routes/productRoutes")
+
 
 const app = express();
+
 app.use(express.json());
+
 app.use(cors());
 
-const PORT = 9000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the E-commerce API!");
-})
+    res.send("Welcome To ShopEase App");
+});
 
-app.listen(PORT, () => {
-    console.log(`🔥🔥🔥Server is running on port http://localhost:${PORT}🔥🔥🔥`);
-})
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server Is Running On http://localhost:${PORT}`);
+} )
