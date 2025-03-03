958b9df7800d139f3300eb13ca5d22c2cbe3eb15
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-03-03 11:31:48 +0100

    Seed The Data

---
+++ backend/seeder.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Product = require("./models/Product");
+const User = require("./models/User");
+const products = require("./data/products");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
+
+const seedData = async () => {
+    try {
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        const createdUser = await User.create({
+            name: "Admin User",
+            email: "admin@example.com",
+            password: "123456",
+            role: "admin",
+        });
+
+        const userID = createdUser._id;
+
+        const sampleProduct = products.map((product) => {
+            return { ...product, user: userID };
+        });
+
+        await Product.insertMany(sampleProduct);
+
+        console.log("Product Data Seeded Successfully");
+        process.exit();
+    } catch (error) {
+        console.error("Error Seeding The Data", error);
+        process.exit(1);
+    }
+};
+
+seedData();
