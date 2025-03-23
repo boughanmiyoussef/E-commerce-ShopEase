6cbd8a05833137b43494696ea5b0dbf4ad5002da
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-03-23 22:04:18 +0100

    Last Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("✅✅✅✅✅MongoDB connected successfully✅✅✅✅✅");
+        console.log("✅✅✅✅MongoDB connected successfully✅✅✅✅");
 // 
     } catch (err) {
         console.error("����������Error connecting to MongoDB����������", err);
