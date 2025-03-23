a51afc251c1ac219bd84255138a3f23674a420e2
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-03-23 22:10:21 +0100

    Last Commit

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-  credentials: true
-}));  app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true
+  })
+);
+app.use(express.json());
 
 dotenv.config();
 
